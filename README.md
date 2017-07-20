ba73083642332dcbaf666f7df8cc5269a47b4fcf
程书婷 Crystal <crystal_cst@qq.com>
2017-07-20 11:46:04 +0800

    select
    selectionSort

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 algorithm exam during learning
 * * *
 ### Sort Algorithm 排序算法
-* * *
+---------------------------------------
 [1.selectionSort 选择排序 demo](https://github.com/crystalcst/algorithm_exam/blob/master/sort/selectionSort.js)</br>
 [2.insertionSort 插入排序 demo](https://github.com/crystalcst/algorithm_exam/blob/master/sort/insersionSort.js)</br>
 [3.mergeSort 归并排序 demo](https://github.com/crystalcst/algorithm_exam/blob/master/sort/mergeSort.js)</br>
--- sort/selectionSort.js
+++ sort/selectionSort.js
@@ -2,16 +2,42 @@
 function selectionSort(arr) {
     var len = arr.length;
     var temp;
-    for(var i = 0;i < len;i++) {
-        for(var j = i+1;j < len;j++) {
-            if(arr[j]<arr[i]) {
+    console.time('选择排序耗时');
+    for (var i = 0; i < len; i++) {
+        for (var j = i + 1; j < len; j++) {
+            if (arr[j] < arr[i]) {
                 temp = arr[i];
                 arr[i] = arr[j];
                 arr[j] = temp;
             }
         }
     }
+    console.timeEnd('选择排序耗时');
     return arr;
 }
 
-selectionSort([4,2,7,8,23,45,767,89,1,2,5,3,67]);
+var arr = [3, 44, 38, 5, 47, 15, 36, 26, 27, 2, 46, 4, 19, 50, 48];
+console.log(selectionSort(arr)); //[2, 3, 4, 5, 15, 19, 26, 27, 36, 38, 44, 46, 47, 48, 50]
+
+//选择排序算法改进，记录位置，循环结束再交换i之后比i大的
+function selectionSort1(arr) {
+    var len = arr.length;
+    var minIndex, temp;
+    console.time('选择排序耗时1');
+    for (var i = 0; i < len - 1; i++) {
+        minIndex = i;
+        for (var j = i + 1; j < len; j++) {
+            if (arr[j] < arr[minIndex]) {     //寻找最小的数
+                minIndex = j;                 //将最小数的索引保存
+            }
+        }
+        temp = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = temp;
+    }
+    console.timeEnd('选择排序耗时1');
+    return arr;
+}
+
+var arr = [3, 44, 38, 5, 47, 15, 36, 26, 27, 2, 46, 4, 19, 50, 48];
+console.log(selectionSort1(arr)); //[2, 3, 4, 5, 15, 19, 26, 27, 36, 38, 44, 46, 47, 48, 50]
